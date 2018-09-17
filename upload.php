26857786b81d76840b56bf2bffde988c49779e38
HIJRATULGEBI PRAMANA <42725375+HijratulgebiPramana@users.noreply.github.com>
2018-09-17 10:19:22 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -5,10 +5,10 @@ $target_file = $target_p. basename($_FILES["fileUpload"]["name"]);
 if(move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file)) {
 	$nama_file = basename($_FILES["fileUpload"]["name"]);
 
-	echo "Sukses Upload" ."<br>";
+	echo "<center> Sukses Upload </center>" ."<br>";
 }else{
 	echo "Gagal Upload";
 }
 ?>
 
-<img width="400px" src="<?php echo $target_p. $nama_file; ?>">
+<center><img width="400px" src="<?php echo $target_p. $nama_file; ?>"></center>
